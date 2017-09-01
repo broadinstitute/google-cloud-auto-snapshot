b04c543fa9e13e96a79ac92ed2aef6743dc78845
David Bernick <dbernick@broadinstitute.org>
2017-09-01 14:09:25 -0400

    Update google-cloud-auto-snapshot.sh

--- google-cloud-auto-snapshot.sh
+++ google-cloud-auto-snapshot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-gcloud compute disks list --project $PROJECT --format='value(name,zone)'|grep $GREPTHING| while read DISK_NAME ZONE; do
-  gcloud compute disks --project $PROJECT snapshot $DISK_NAME --snapshot-names autogcs-${DISK_NAME:0:31}-$(date "+%Y-%m-%d-%s") --zone $ZONE
+gcloud compute disks list --project $2 --format='value(name,zone)'|grep $1| while read DISK_NAME ZONE; do
+  gcloud compute disks --project $2 snapshot $DISK_NAME --snapshot-names autogcs-${DISK_NAME:0:31}-$(date "+%Y-%m-%d-%s") --zone $ZONE
 done
 #
 # snapshots are incremental and dont need to be deleted, deleting snapshots will merge snapshots, so deleting doesn't loose anything
 # having too many snapshots is unwiedly so this script deletes them after 60 days
 #
-gcloud compute snapshots list --project $PROJECT --filter="creationTimestamp<$(date --date="30 days ago" +"%Y-%m-%d")" --regexp "(autogcs.*)" --uri | while read SNAPSHOT_URI; do
-   gcloud compute snapshots --project $PROJECT delete $SNAPSHOT_URI
+gcloud compute snapshots list --project $2 --filter="creationTimestamp<$(date --date="30 days ago" +"%Y-%m-%d")" --regexp "(autogcs.*)" --uri | while read SNAPSHOT_URI; do
+   gcloud compute snapshots --project $2 delete $SNAPSHOT_URI
 done
 #
